e5dc33694bccebd8f469f2df0a622342a7afd146
ultimecreation <naserbechata@gmail.com>
2022-08-17 10:04:38 +0200

    added header background + style

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Header.scss'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='header'>
+      <figure>
+        <div className="overlay">
+          <img src={`${process.env.PUBLIC_URL}/images/background.png`} alt="" />
+        </div>
+        <figcaption>Chez vous, partout et ailleurs</figcaption>
+      </figure>
     </div>
   )
 }
