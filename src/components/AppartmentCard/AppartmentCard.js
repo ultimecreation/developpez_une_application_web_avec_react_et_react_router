9e0f5662ff16b53985b2d240bcde8b08bf018019
ultimecreation <naserbechata@gmail.com>
2022-08-19 11:59:35 +0200

    added modal

--- src/components/AppartmentCard/AppartmentCard.js
+++ src/components/AppartmentCard/AppartmentCard.js
@@ -3,11 +3,16 @@ import { Link } from 'react-router-dom'
 import './AppartmentCard.scss'
 
 const AppartmentCard = (props) => {
+  const openModalAndSetCurrentIndex = () => {
+    props.handleShowModal()
+    props.handleSetcurrentIndex(props.currentIndex)
+  }  
+
   return (
     <div className='card'>
 
       <figure>
-        <img src={props.cover} alt={props.title} />
+        <img src={props.cover} alt={props.title} onClick={openModalAndSetCurrentIndex} />
         <Link to={`/appartment-details/${props.id}`}>
           <figcaption>{props.title}</figcaption>
         </Link>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,50 @@
 import './Home.scss'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Footer from '../../layout/Footer/Footer'
 import Header from '../../layout/Header/Header'
-import {AppartmentsContext} from '../../context/AppartmentsContext'
+import { AppartmentsContext } from '../../context/AppartmentsContext'
 import AppartmentCard from '../../components/AppartmentCard/AppartmentCard'
+import Modal from '../../components/Modal/Modal'
 
 const Home = () => {
     const appartmentsContext = useContext(AppartmentsContext)
-    const {appartments} = appartmentsContext
+    const { appartments } = appartmentsContext
 
-    console.log(appartments)
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const slides = appartments.reduce((accumulator,currentAppartment) => {
+        accumulator.push(currentAppartment.cover)
+        return accumulator
+    },[])
+    
+    const showModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const hideModal = () => {
+        setModalIsOpen(false);
+    };
     return (
         <div className='container'>
-            <Header />
+            <Header imgSource={`${process.env.PUBLIC_URL}/images/background.png`}/>
             <div className="appartment-container">
-                { appartments.map(appartment => {
-                   return <AppartmentCard 
-                            id={appartment.id}
-                            title={appartment.title} 
-                            cover={appartment.cover}
-                        />
-                   
+                {appartments.map((appartment,index) => {
+                    return <AppartmentCard
+                        id={appartment.id}
+                        title={appartment.title}
+                        cover={appartment.cover}
+                        handleShowModal={showModal}
+                        currentIndex={index}
+                        handleSetcurrentIndex={setCurrentIndex}
+                        key={index}
+                    />
+
                 })
                 }
-                </div>
+            </div>
             <Footer />
+            <Modal show={modalIsOpen} handleClose={hideModal} items={slides} currentIndex={currentIndex}/>
         </div>
     )
 }
